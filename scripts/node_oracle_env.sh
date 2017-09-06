ac3d4bf657822aac92853bb1071b851ccf7ccc4f
Jerry Thomas <me@jerrythomas.name>
2017-09-06 09:27:28 +0530

    Workaround the environment variables  problem.

--- scripts/node_oracle_env.sh
+++ scripts/node_oracle_env.sh
@@ -4,10 +4,12 @@
 #sudo apt-get update -qq
 #sudo apt-get install -qq libaio1
 echo "Setting up oracle instant client and sdk"
-pwd
 curl https://raw.githubusercontent.com/CollinEstes/docker-node-oracle/master/oracle/linux/instantclient-basic-linux.x64-12.1.0.2.0.zip | python -c "import zipfile,sys,StringIO;zipfile.ZipFile(StringIO.StringIO(sys.stdin.read())).extractall(sys.argv[1] if len(sys.argv) == 2 else '.')" ./
 curl https://raw.githubusercontent.com/CollinEstes/docker-node-oracle/master/oracle/linux/instantclient-sdk-linux.x64-12.1.0.2.0.zip | python -c "import zipfile,sys,StringIO;zipfile.ZipFile(StringIO.StringIO(sys.stdin.read())).extractall(sys.argv[1] if len(sys.argv) == 2 else '.')" ./
 
+# Workaround the env problem.
+sudo mkdir -p /opt/oracle
+sudo ln -s instantclient_12_1 /opt/oracle/instantclient
 pushd .
 cd instantclient_12_1
 for file in *.so.*; do
@@ -22,3 +24,5 @@ export NLS_LANG=.UTF8 # Optional, but required to support international characte
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$OCI_LIB_DIR"
 
 popd
+
+echo "OCI home is : $OCI_HOME"
