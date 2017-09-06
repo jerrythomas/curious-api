5de217df5bda9a797d3da6b78682939afdb36eef
Jerry Thomas <me@jerrythomas.name>
2017-09-06 09:30:15 +0530

    removed sudo

--- scripts/node_oracle_env.sh
+++ scripts/node_oracle_env.sh
@@ -8,8 +8,8 @@ curl https://raw.githubusercontent.com/CollinEstes/docker-node-oracle/master/ora
 curl https://raw.githubusercontent.com/CollinEstes/docker-node-oracle/master/oracle/linux/instantclient-sdk-linux.x64-12.1.0.2.0.zip | python -c "import zipfile,sys,StringIO;zipfile.ZipFile(StringIO.StringIO(sys.stdin.read())).extractall(sys.argv[1] if len(sys.argv) == 2 else '.')" ./
 
 # Workaround the env problem.
-sudo mkdir -p /opt/oracle
-sudo ln -s instantclient_12_1 /opt/oracle/instantclient
+mkdir -p /opt/oracle
+ln -s instantclient_12_1 /opt/oracle/instantclient
 pushd .
 cd instantclient_12_1
 for file in *.so.*; do
