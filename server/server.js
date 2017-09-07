a59a284138bff259b08a2d25166893284ea7a303
Jerry Thomas <me@jerrythomas.name>
2017-09-07 17:07:36 +0530

    Postgres query handler works but requires an environment variable

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const Hapi = require('hapi');
 const Joi = require('joi');
+const pgConn = require('hapi-postgres-connection');
 
 const server = new Hapi.Server();
 
 require('env2')('.env'); // loads all entries into process.env
+DATABASE_URL=process.env.DATABASE_URL
 
 server.connection({
   port: process.env.PORT,
   host: 'localhost',
+});
+
+server.register({ // register all your plugins
+  register: pgConn, // no options required
+}, (err) => {
+  if (err) {
+    console.error(err);
+  }
 });
 
 server.route({
@@ -25,6 +35,22 @@ server.route({
     },
     handler(req, reply) {
       reply(`Hello ${req.params.user}!`);
+    },
+  },
+});
+
+server.route({
+  method: 'GET',
+  path: '/api/v1/postgres',
+  config: {
+    handler(request, reply) {
+      const payload = request.headers;
+      console.log(payload);
+
+      request.pg.client.query(payload.query, (err, result) => {
+        console.log(err, result);
+        return reply(result.rows[0]);
+      });
     },
   },
 });
